e2de5cdf56f278bf1592b07f0448546f54597e3d
Alberto <sinspiral@gmail.com>
2011-05-19 12:42:53 +0200

    Adding current state of webpage with layouts

---
+++ js/init.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	/* Fancybox */	
+	$("a.imagelink").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'opacity'		:	true, 
+		'speedIn'		:	400, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	true,
+		'overlayOpacity': 	0.85,
+		'overlayColor': "#000",
+		'titlePosition': "inside"
+	});
+	
+});
